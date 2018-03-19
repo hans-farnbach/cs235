521f7c27a1524d2d11bf0a8f0e450cbaffbd6d49
hans-farnbach <hans@farnbach.net>
2018-03-19 14:06:03 -0600

    Commenting

--- Lab2.cpp
+++ Lab2.cpp
@@ -1,3 +1,4 @@
+// "Spell-checker", using a given list of words, find any words in a file that aren't on that list
 #include <fstream>
 #include <vector>
 #include <set>
@@ -10,30 +11,27 @@ using namespace std;
 
 string toLowerCase(string word){
     for (int i = 0; i < word.length(); i++){
-        if (word[i] >= 'A' && word[i] <= 'Z')
-            word[i] += 'a' - 'A';
-        else if (!(word[i] >= 'a' && word[i] <= 'z') && !(word[i] == ' '))
-            word[i] = '\0';
+        if (word[i] >= 'A' && word[i] <= 'Z')	// If it's a capital letter
+            word[i] += 'a' - 'A';		// shift it up the ASCII chart to the lower case portion
+        else if (!(word[i] >= 'a' && word[i] <= 'z') && !(word[i] == ' '))	// If it's not a lower case letter or a space
+            word[i] = '\0';							// delete it
     }
     return word;
 }
 
 int main(int argc, const char* argv[]){
     ifstream reader;
-    ofstream writer(argv[3]);
+    ofstream writer(argv[3]);	// output file
 
-    reader.open(argv[1]);
+    reader.open(argv[1]); 	// dictionary file
 
     string line;
     
     set<string> dict;
 
-    while (getline(reader, line)){        
-        for (int i = 0; i < line.length(); i++){
-            if (line[i]>= 'A' && line[i] <= 'Z')
-                line[i] += 'a' - 'A';
-        }
-            
+    // go through the entire dictionary and lowercase it for easy comparisons
+    while (getline(reader, line)){
+        line = toLowerCase(line);            
         dict.insert(line);
     }
     
@@ -41,27 +39,29 @@ int main(int argc, const char* argv[]){
     
     map<string, list<int>> misspelled;
 	
-	reader.open(argv[2]);
+    reader.open(argv[2]); 	// thing to spellcheck
 
     string word;
     stringstream wordReader;
     
-    int lineNum = 1;
+    int lineNum = 1;		// have to keep track of which line a misspelled word appeared on
     
     while (getline (reader, line)){
-        wordReader.clear();
+        wordReader.clear();	// empty the buffer
         wordReader.str("");
         
         line = toLowerCase(line);
         
         wordReader << line;
 
+	// go through each word on the line and check if it's in the dictionary
         while (wordReader >> word)
             if (!dict.count(word))
-                misspelled[word].push_back(lineNum);
-		lineNum++;
+                misspelled[word].push_back(lineNum);	// push ones that aren't in the dictionary with a line number
+	lineNum++;
     }
 
+    // output it all
     for(map<string, list<int>>::iterator iMap = misspelled.begin(); iMap != misspelled.end(); iMap++){
         writer << iMap->first << ":";
         for (list<int>::iterator iList = iMap->second.begin(); iList != iMap->second.end(); iList++)
