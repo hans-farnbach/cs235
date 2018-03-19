544dc69b00f212eaafbb2b48df9f296e925fd1f7
hans-farnbach <hans@farnbach.net>
2018-03-19 16:54:06 -0600

    Commenting

--- Lab3.cpp
+++ Lab3.cpp
@@ -1,5 +1,7 @@
-#include <fstream>
-#include <iostream>
+// Boggle. Accepts a board file and a dictionary file, then recursively searches the board for those words.
+
+#include <fstream>	// read a file
+#include <iostream>	// 
 #include <string>
 #include <set>
 #include <vector>
@@ -9,6 +11,7 @@ using namespace std;
 
 set<string> found;
 
+// converts a string to all lowercase letters
 string toLowerCase(string str) {
 	for (int i = 0; i < str.size(); i++) {
 		if (str[i] >= 'A' && str[i] <= 'Z')
@@ -21,6 +24,12 @@ string toLowerCase(string str) {
 	return str;
 }
 
+// creates the actual board by making a square and padding the edges with 0s
+// 0 0 0 0 0
+// 0 e x a 0
+// 0 m p l 0
+// 0 e b o 0
+// 0 0 0 0 0
 vector<vector<string>> buildBoard(const char* file) {
 	ifstream boardReader;
 	boardReader.open(file);
@@ -32,14 +41,15 @@ vector<vector<string>> buildBoard(const char* file) {
 		letters = toLowerCase(letters);
 		for (int i = 0; i < letters.size(); i++) {
 			letter = letters[i];
-			if (letters[i] == 'q' && letters[i + 1] == 'u')
+			if (letter == 'q' && letters[i + 1] == 'u') // keep "qu" together
 				letter = letters[i] + letters[++i];
 			charList.push_back(letter);
 		}
 	}
 
-	int square = sqrt(charList.size());
+	int square = sqrt(charList.size());	// figure out the dimensions for the board, keeping it square
 	
+	// create each row
 	for (int row = 0; row < square; row++) {
 		vector<string> rows;
 		for (int element = 0; element < square; element++)
@@ -51,8 +61,9 @@ vector<vector<string>> buildBoard(const char* file) {
 	return board;
 }
 
-bool flagNabIt(string word, set<string> dictionary, int row, int column, vector<vector<bool>> urAFlagit) {
-	if (urAFlagit[row][column])
+// keep track of letters we've visited and if they are valid paths to follow
+bool flagNabIt(string word, set<string> dictionary, int row, int column, vector<vector<bool>> Flagit) {
+	if (Flagit[row][column])
 		return true;
 	string prefix = word;
 	prefix[prefix.length() - 1]++;
@@ -63,29 +74,30 @@ bool flagNabIt(string word, set<string> dictionary, int row, int column, vector<
 	return false;
 }
 
-void boggleMyMind(set<string> dictionary, vector<vector<string>> board, int row, int column, string word, vector<vector<bool>> urAFlagit) {
+// run the game
+void boggleMyMind(set<string> dictionary, vector<vector<string>> board, int row, int column, string word, vector<vector<bool>> Flagit) {
 	if (row < 0 || row >= board.size() || column < 0 || column >= board.size())
 		return;
 
 	string newWord = word + board[row][column];
-	if (flagNabIt(newWord, dictionary, row, column, urAFlagit))
+	if (flagNabIt(newWord, dictionary, row, column, Flagit))
 		return;
 
-	if (dictionary.count(newWord) && newWord.length() >= 4)
+	if (dictionary.count(newWord) && newWord.length() >= 4)	// Don't allow words less than 4 characters long
 		found.insert(newWord);
 	
-	urAFlagit[row][column] = true;
+	Flagit[row][column] = true;	// Flag the current block as a visited one
 
-	boggleMyMind(dictionary, board, row, (column-1), newWord, urAFlagit);
-	boggleMyMind(dictionary, board, row, (column+1), newWord, urAFlagit);
-	boggleMyMind(dictionary, board, (row-1), column, newWord, urAFlagit);
-	boggleMyMind(dictionary, board, (row-1), (column-1), newWord, urAFlagit);
-	boggleMyMind(dictionary, board, (row-1), (column+1), newWord, urAFlagit);
-	boggleMyMind(dictionary, board, (row+1), column, newWord, urAFlagit);
-	boggleMyMind(dictionary, board, (row+1), (column-1), newWord, urAFlagit);
-	boggleMyMind(dictionary, board, (row+1), (column+1), newWord, urAFlagit);
+	boggleMyMind(dictionary, board, row, (column-1), newWord, Flagit);	// Left
+	boggleMyMind(dictionary, board, row, (column+1), newWord, Flagit);	// Right
+	boggleMyMind(dictionary, board, (row-1), column, newWord, Flagit);	// Down
+	boggleMyMind(dictionary, board, (row-1), (column-1), newWord, Flagit);	// Down left
+	boggleMyMind(dictionary, board, (row-1), (column+1), newWord, Flagit);	// Down right
+	boggleMyMind(dictionary, board, (row+1), column, newWord, Flagit);	// Up
+	boggleMyMind(dictionary, board, (row+1), (column-1), newWord, Flagit);	// Up left
+	boggleMyMind(dictionary, board, (row+1), (column+1), newWord, Flagit);	// Up right
 
-	urAFlagit[row][column] = false;
+	Flagit[row][column] = false;	// Unflag it
 }
 
 int main(int argc, const char* argv[]) {
@@ -113,7 +125,7 @@ int main(int argc, const char* argv[]) {
 		flagalicious.push_back(rows);
 	}
 
-	// Biggle boogle boggle while outputting board to file
+	// Do the Boggle and output board to file
 	ofstream resultWriter;
 	resultWriter.open(argv[3]);
 	word = "";
@@ -127,6 +139,7 @@ int main(int argc, const char* argv[]) {
 		
 	resultWriter << endl;
 
+	// output the words found
 	for (set<string>::iterator i = found.begin(); i != found.end(); i++)
 		resultWriter << *i << endl;
 
