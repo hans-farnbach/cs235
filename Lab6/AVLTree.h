3ba87f1bde198bfcd7f275133bf54092b99bf56d
hans-farnbach <hans@farnbach.net>
2018-03-19 23:19:40 -0600

    Commenting and fixing the spacing

--- Lab6/AVLTree.h
+++ Lab6/AVLTree.h
@@ -3,12 +3,11 @@
 #include <iostream>
 #include <fstream>
 #include <algorithm>
-
 #include "LinkedList.h"
 
 using namespace std;
 
-template <typename T>
+template <typename T>	// allow an AVL tree for any data type
 
 class AVLTree {
   public:
@@ -16,10 +15,10 @@ class AVLTree {
     public:
         Node():left(NULL), right(NULL), height(1) {}
 
-        T value;
-        Node* left;
-        Node* right;
-        int height;
+        T value;	// the node itself
+        Node* left;	// left child of the node
+        Node* right;	// right child of the node
+        int height;	// how tall the AVL tree is
     };
     
     Node* root;
@@ -27,6 +26,7 @@ class AVLTree {
     
     AVLTree():root(NULL){}
     
+    // add a value to the tree
     Node* add(const T& value, Node* node){
         if (node == NULL){
             node = new Node;
@@ -46,21 +46,20 @@ class AVLTree {
         
         return node;
     }
-    
+    // balance the tree
     Node* balance(Node* toBalance){
         int cDiff = findDiff(toBalance);
+	// right side too heavy
         if (cDiff < -1){
             if (findDiff(toBalance->right) > 0)
                 toBalance = doubLeftRot(toBalance);
             else
                 toBalance = leftRot(toBalance);
-            
             if(toBalance->left != NULL)
                 toBalance->left->height = getHeight(toBalance->left);
             toBalance->height = getHeight(toBalance);
-            
-
         }
+	// left side too heavy
         else if (cDiff > 1){
             if (findDiff(toBalance->left) < 0)
                 toBalance = doubRightRot(toBalance);
@@ -72,11 +71,10 @@ class AVLTree {
             toBalance->height = getHeight(toBalance);
 
         }
-        
-        
         return toBalance;
     }
     
+    // find height difference between left side and right side of a given node
     int findDiff(Node* temp){
         if (temp == NULL)
             return 0;
@@ -84,16 +82,16 @@ class AVLTree {
         int rightHeight = getHeight(temp->right);
         return (leftHeight - rightHeight);
     }
-       
+    // find the height of a given node
     int getHeight(Node* node){
         if (node == NULL)
             return 0;
         else if (getHeight(node->right) >= getHeight(node->left))
-			return (getHeight(node->right) + 1);
-		else
+	    return (getHeight(node->right) + 1);
+	else
             return (getHeight(node->left) + 1);
     }
-    
+    // do a right rotation
     Node* rightRot(Node* node){
         Node* childL;
         childL = node->left;
@@ -104,32 +102,21 @@ class AVLTree {
         
         return childL;
     }
-
+    // do a double right rotation
     Node* doubRightRot(Node* node) {
         Node* childL = node->left;
-        
         Node* childLR = childL->right;
-        
         node->left = childLR;
-        
         childL->right = childLR->left;
-        
         childLR->left = childL;
-        
         node->left = childLR->right;
-        
         childLR->right = node;
-        
         node->height = getHeight(node);
-        
         childL->height = getHeight(childL);
-        
         childLR->height = getHeight(childLR);
-        
-        
         return childLR;
     }
-
+    // do a left rotation
     Node* leftRot(Node* node){
         Node* childR;
         childR = node->right;
@@ -137,76 +124,63 @@ class AVLTree {
         childR->left = node;
         return childR;
     }
-
+    // do a double left rotation
     Node* doubLeftRot(Node* node){
         Node* childR = node->right;
-        
         Node* childRL = childR->left;
-        
         childR->left = childRL->right;
-        
         node->right = childRL;
-        
         childRL->right = childR;
-        
-        
         node->right = childRL->left;
-        
         childRL->left = node;
-        
         node->height = getHeight(node);
-        
         childR->height = getHeight(childR);
-        
         childRL->height = getHeight(childRL);
-        
-        
         return childRL;
     }
-    
+   
+   // remove(1) removes the root
    void remove(const T& value){
        root = remove(value, root);
    }
-    
-    Node* remove(const T& value, Node* node)
-    {
-        if (node == NULL)
-            return NULL;
-        if (value < node->value)
-            node->left = remove(value, node->left);
-        else if (value > node->value)
-            node->right = remove(value, node->right);
-        else {
-            Node* L = node->left;
-            Node* R = node->right;
- 
-            if (L) { // if L && R
-				if (R) {
-					node->value = CTR(node->right); //Findmin(current->right) (goes all the way left)
-					node->right = remove(node->value, node->right);
-				}
-				else {
-					delete node;
-					return L;
-				}
-            }
+   
+   // remove(2) pops a given node
+   Node* remove(const T& value, Node* node)
+   {
+       if (node == NULL)
+           return NULL;
+       if (value < node->value)
+           node->left = remove(value, node->left);
+       else if (value > node->value)
+           node->right = remove(value, node->right);
+       else {
+           Node* L = node->left;
+           Node* R = node->right;
+           if (L) {
+		if (R) {
+	 	    node->value = CTR(node->right);    //Findmin(current->right) (goes all the way left)
+		    node->right = remove(node->value, node->right);
+		}
+		else {
+		    delete node;
+		    return L;
+		}
+           }
             
-            else if(R){
-                delete node;
-                return R;
-            }
+           else if(R){
+               delete node;
+               return R;
+           }
             
-            else{
-                delete node;
-                return NULL;
-            }
-        }
-        
-        node->height = getHeight(node);
-        
-        return balance(node);
+           else{
+               delete node;
+               return NULL;
+           }
+       }
+       node->height = getHeight(node);
+       return balance(node);
     }
-    
+    // handle a case where you remove a node with two children
     Node* twoChildHandler(T value, Node* node){
         Node* L = node->left;
         Node* R = node->right;
@@ -215,32 +189,34 @@ class AVLTree {
             return NULL;
         }
         
+	// if children on both sides
         else if(L && R){
             node->value = CTR(node->right);
             node->right = remove(node->value, node->right);
         }
-        
+        // if a child on the right
         else if(R){
             delete node;
             return R;
         }
+	// else a child on the left
         else {
             delete node;
             return L;
         }
     }
-    
+    // find the leftmost node
     T CTR(Node* node){
         if (node->left == NULL)
             return node->value;
         else
             return CTR(node->left);
     }
-    
+    // find(1) returns the root
     bool find(const T& value){
         return find(value, root);
     }
-    
+    // find(2) will find a node based on its value
     bool find(const T& value, Node* node){
         if (node == NULL)
             return false;
@@ -254,16 +230,12 @@ class AVLTree {
         else
             return (find(value, node->right));
     }
-    
+    // output the AVL tree
     void print(ofstream &writer){
-
         writer << "print" << endl;
-        
         if (root == NULL)
             return;
-        
         LinkedList<AVLTree::Node*> queue;
-		
         queue.insert(queue.getSize(), root);
 
         int level = 0;
@@ -272,38 +244,29 @@ class AVLTree {
             AVLTree::Node* temp;
             writer << "level " << level << ": ";
             linefill = 0;
-            for (int levelsize = queue.getSize(); levelsize > 0; levelsize--){
-                
+            for (int levelsize = queue.getSize(); levelsize > 0; levelsize--) {
                 temp = queue.getNode(0);
                 queue.remove(0);
-
                 if (linefill >= 8){
                     writer << endl << "level " << level << ": ";
                     linefill = 0;
                 }
                 writer << temp->value << "(" << temp->height << ") ";
-                
                 if (temp->left != NULL)
                     queue.insert(queue.getSize(), temp->left);
                 if (temp->right != NULL)
                     queue.insert(queue.getSize(), temp->right);
-                
-
-                
                 linefill++;
             }
-            
             level++;
-            
             writer << endl;
         }
     }
-    
+    // delete the AVL tree
     void clear(){
         while(root != NULL)
             remove(root->value);
     }
-    
     ~AVLTree(){
         clear();
     }
